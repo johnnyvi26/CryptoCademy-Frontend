80937e77370b10c773f59631b0a047c8d7bc525b
SnakeBearLB <79704111+SnakeBearLB@users.noreply.github.com>
2021-11-08 11:53:05 -0500

    created styles folder. styling to porfolio and header pages.

---
+++ src/styles/Header.styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components"
+
+export const Nav = styled.nav`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  margin-top: 20px;
+  margin-bottom: 40px;
+`
