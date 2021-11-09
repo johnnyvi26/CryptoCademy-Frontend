e5ff7fea26ea737ac0b666dd50e21341a0e1554a
Oshprengel <oshprengel@gmail.com>
2021-11-09 14:06:25 -0700

    styled trade page

---
+++ src/styles/Trade.styled.js
@@ -0,0 +1,38 @@
+import styled from "styled-components"
+
+export const All = styled.button`
+background-color:red;
+`
+export const Option = styled.select`
+height: 40px;
+`
+export const Number = styled.input`
+height: 40px;
+`
+
+export const Submit = styled.input`
+height: 40px;
+`
+
+export const Arrow = styled.span`
+font-size: 50px;
+margin: 0px 0px;
+position: relative;
+top:10px;
+`
+
+export const Error = styled.div`
+color:red;
+`
+
+export const Success = styled.div`
+
+`
+
+export const Balance =styled.div`
+
+`
+
+export const Form = styled.form`
+
+`
