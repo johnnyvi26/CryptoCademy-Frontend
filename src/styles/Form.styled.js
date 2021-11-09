c9bff9d668e6bf9f3cfec5f99c1807da1e19cc33
Mavi Bucheli <mavibucheli@gmail.com>
2021-11-09 12:04:06 -0500

    finished styling login and newuser

---
+++ src/styles/Form.styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components"
+
+export const Div = styled.div`
+text-align: center;
+margin-top: 250px;
+margin-bottom: 40px;
+padding-top: 100px;
+`
+export const Input = styled.input`
+margin-bottom: 10px;
+`
