b312b99410176f32cd831c81aa8df28308136252
Mavi Bucheli <mavibucheli@gmail.com>
2021-11-09 12:34:31 -0500

    fixed styling for login and newuser

--- src/styles/Login.styled.js
+++ src/styles/Login.styled.js
@@ -2,12 +2,14 @@ import styled from "styled-components"
 
 export const Div = styled.div`
 text-align: center;
-background-color: #77CDB4;
+border: 10px solid #77CDB4;
+border-radius: 5px;
 padding-bottom: 100px;
 box-shadow: 15px 15px rgb(0, 0, 0, 0.3);
-margin-left: 60px;
-margin-right: 60px;
+margin-left: 40px;
+margin-right: 40px;
 font-family: Cambria;
+margin-top: 160px;
 `
 export const P = styled.p`
 font-size: 16px;
